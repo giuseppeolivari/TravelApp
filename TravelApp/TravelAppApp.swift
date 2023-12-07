09a64f970d5c67ae050537f7b6e2610b9a49bea8
Giuseppe <golivari1@gmail.com>
2023-12-07 12:02:09 +0100

    Initial commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Giuseppe Olivari on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
