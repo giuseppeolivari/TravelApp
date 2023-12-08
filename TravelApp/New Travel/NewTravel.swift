826a1e9e372c9548b92cd575dcf828be7af01b9c
Giuseppe <golivari1@gmail.com>
2023-12-08 19:08:45 +0100

    calendar

--- TravelApp/New Travel/NewTravel.swift
+++ TravelApp/New Travel/NewTravel.swift
@@ -8,13 +8,71 @@
 import SwiftUI
 
 struct NewTravel: View {
+    
+    @State  var startDate = Date()
+    @State  var endDate = Date()
+    
+    
     var body: some View {
-        VStack{
-            
-            
+        VStack {
+                    
+            /*
+            .sheet(isPresented: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is Presented@*/.constant(false)/*@END_MENU_TOKEN@*/) {
+                Group{
+                    //Text("data iniziale")
+                    DatePicker("From", selection: $startDate, displayedComponents: .date)
+                        //.datePickerStyle(WheelDatePickerStyle())
+                        .padding()
+                    
+                    //Text("finale")
+                    DatePicker("To", selection: $endDate, displayedComponents: .date)
+                    //.datePickerStyle(GraphicalDatePickerStyle())
+                        .padding()
+                    
+                    Text("Giorni trascorsi: \(calculateDaysBetweenDates())")
+                        .padding()
+                    
+                }
+            }
+            */
+
+            Group{
+                //Text("data iniziale")
+                DatePicker("From", selection: $startDate, displayedComponents: .date)
+                    //.datePickerStyle(WheelDatePickerStyle())
+                    .padding()
+                
+                //Text("finale")
+                DatePicker("To", selection: $endDate, displayedComponents: .date)
+                //.datePickerStyle(GraphicalDatePickerStyle())
+                    .padding()
+                
+                Text("Giorni trascorsi: \(calculateDaysBetweenDates())")
+                    .padding()
+                
+            }
+                
         }
     }
+    
+    
+    
+    func calculateDaysBetweenDates() -> Int {
+            let calendar = Calendar.current
+            let start = calendar.startOfDay(for: startDate)
+            let end = calendar.startOfDay(for: endDate)
+            let components = calendar.dateComponents([.day], from: start, to: end)
+            return components.day ?? 0
+        }
+    
+    
+    
+    
+    
+    
 }
+    
+
 
 #Preview {
     NewTravel()
