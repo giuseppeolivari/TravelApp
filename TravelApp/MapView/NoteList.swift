dfc8ff7c7de60a20ca536c546b5c2bd362c1ee3f
Ali Asgari <ali8asgari@gmail.com>
2023-12-12 10:31:51 +0100

    NoteList page.

--- TravelApp/MapView/NoteList.swift
+++ TravelApp/MapView/NoteList.swift
@@ -9,11 +9,50 @@ import SwiftUI
 
 struct NoteList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Header with a picture
+            Image("Header")
+                .resizable()
+                              .scaledToFill()
+                              .frame(maxWidth: .infinity, maxHeight: 200)
+                              .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                              .edgesIgnoringSafeArea(.top)
+                              .clipped()
+                              .cornerRadius(15)
+            
+            
+            
+
+            // List of clickable dates
+            List {
+                ForEach(1..<18) { index in
+                    // Replace "Your Date" with the actual date information
+                    NavigationLink(destination: Text("Details for Date \(index)")) {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Your Date \(index)")
+                                .font(.headline)
+                                .foregroundColor(.primary)
+                            Text("Additional information or details for date \(index)")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        }
+                        .padding(10)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+        }
     }
 }
 
-#Preview {
-    NoteList()
+struct NoteList_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteList()
+    }
 }
+
+
 //ALI YOU ARE BEAUTIFUL
