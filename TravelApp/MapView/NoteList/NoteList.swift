2bc6f46dbdb04585a0d817190691b5e475211d9c
Giuseppe <golivari1@gmail.com>
2023-12-12 09:26:47 +0100

    newpageALI

---
+++ TravelApp/MapView/NoteList/NoteList.swift
@@ -0,0 +1,18 @@
+//
+//  NoteList.swift
+//  TravelApp
+//
+//  Created by Giuseppe Olivari on 12/12/23.
+//
+
+import SwiftUI
+
+struct NoteList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteList()
+}
