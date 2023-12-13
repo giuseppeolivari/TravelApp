8617797156fb3b013800435856632589e6ce6543
giuseppeolivari <golivari1@gmail.com>
2023-12-13 12:09:57 +0100

    adriano page

--- TravelApp/FirstPage/MainPage.swift
+++ TravelApp/FirstPage/MainPage.swift
@@ -1,4 +1,4 @@
-//
+
 //  MainPage.swift
 //  TravelApp
 //
@@ -9,37 +9,38 @@ import SwiftUI
 import UIKit
 
 struct MainPage: View {
-    let data = (1...100).map {"Pezzi\($0)"}
+    let data = (1...20).map {"Pezzi\($0)"}
+    let image1 = "mappa"
+    var bool : Bool = true
+  
     
-
     var body: some View{
         
-        NavigationView{
+        NavigationStack{
             ScrollView{
-                LazyVGrid(columns: [GridItem(.fixed(100)),GridItem()], content: {
+                LazyVGrid(columns: [/*GridItem(spacing: 10),*/GridItem(.flexible(), spacing: 10)], content: {
                     ForEach(data, id: \.self){pezzo in
-                      Image("mappa")
-                            .opacity(1) //0.4
-                            .cornerRadius(230)
+                        if bool {
+                            NavigationLink(destination: TravelApp.NewTravel(), label:{
+                                
+                                Image(image1)
+                                    .resizable()
+                                    .opacity(0.7) //0.4
+                                    .cornerRadius(20)
+                                    .frame(width: 350, height: 200)
+                                    .padding()
+                                    .overlay(
+                                        Image(systemName: "plus.circle")
+                                            .resizable()
+                                            .opacity(0.6)
+                                            .frame(width: 60, height: 60,alignment: .bottomTrailing)
+                                            .tint(.black)
+                                    )})
+                        } else{
+                            Text("CIAO")
+                        }
                         
                     }
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
                     
                 }).navigationTitle("Hello").toolbar(){
                     ToolbarItem(placement: .topBarTrailing) {
@@ -75,4 +76,3 @@ struct MainPage: View {
 #Preview {
     MainPage()
 }
-
