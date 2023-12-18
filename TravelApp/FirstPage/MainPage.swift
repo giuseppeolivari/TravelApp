0fd9a79aa53a80bcefb070525381e9f5c94d12f9
xHero <loergame@gmail.com>
2023-12-18 09:21:13 +0100

    Fix Map

--- TravelApp/FirstPage/MainPage.swift
+++ TravelApp/FirstPage/MainPage.swift
@@ -34,7 +34,7 @@ struct MainPage: View {
                             }
                         }, label: {
                             ZStack {
-                                Image("mappa")
+                                Image("mappaplus")
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
                                     .cornerRadius(30)
